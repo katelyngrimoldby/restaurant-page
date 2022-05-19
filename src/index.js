0c0c029759eca3fbd5394faad9b5d5966e5a0022
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-05-19 13:55:05 -0600

    Add tabbing behaviour

--- src/index.js
+++ src/index.js
@@ -4,13 +4,36 @@ import {menu} from './components/menu.js';
 import {contact} from './components/contact.js';
 import {footer} from './components/footer.js';
 
-const content = document.getElementById('content')
-const load = (() => {
-    content.appendChild(heading);
-    content.appendChild(home);
-    content.appendChild(menu);
-    content.appendChild(contact);
-    content.appendChild(footer);
-})();
+(() => {
+    //initial page load
+    const content = document.getElementById('content');
+    content.append(heading, home, footer);
 
-load
+    //btn variables
+    const homeBtn = document.getElementById('homeBtn');
+    const menuBtn = document.getElementById('menuBtn');
+    const conatctBtn = document.getElementById('contactBtn');
+
+    //event listeners
+    homeBtn.addEventListener('click', () => {
+        //removes current tab
+        heading.nextElementSibling.remove();
+
+        //inputs specified tab
+        heading.insertAdjacentElement('afterend', home);
+    })
+    menuBtn.addEventListener('click', () => {
+        //removes current tab
+        heading.nextElementSibling.remove();
+
+        //inputs specified tab
+        heading.insertAdjacentElement('afterend', menu);
+    })
+    contactBtn.addEventListener('click', () => {
+        //removes current tab
+        heading.nextElementSibling.remove();
+
+        //inputs specified tab
+        heading.insertAdjacentElement('afterend', contact);
+    })
+})();
