4627059fdcd5eb5188d4ff31c2df09be6489b1de
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-05-19 13:09:04 -0600

    Add contact tab

---
+++ src/components/contact.js
@@ -0,0 +1,60 @@
+const hero = (() => {
+    //create elements
+    const container = document.createElement('section');
+    const title = document.createElement('h1');
+
+    //add content
+    container.classList.add('hero');
+    title.textContent = 'Contact Us';
+
+    //append to container
+    container.appendChild(title);
+
+    return container;
+})();
+
+const content = (() => {
+    //create elements
+    const container = document.createElement('div');
+    const location = document.createElement('section');
+    const info = document.createElement('section');
+    const locationH = document.createElement('h2');
+    const locationS = document.createElement('span');
+    const infoH = document.createElement('h2');
+    const phone = document.createElement('span');
+    const email = document.createElement('a');
+
+    //add content
+    container.classList.add('content');
+    location.classList.add('card');
+    info.classList.add('card');
+
+    locationH.textContent = 'Location';
+    locationS.textContent = '123 Main Street, Somewhereville AB';
+
+    infoH.textContent = 'Reach Out';
+    phone.textContent ='(555) 123-4567';
+    email.setAttribute('href','mailto:#');
+    email.setAttribute('target', '_blank');
+    email.textContent = 'Click to email';
+
+    //append children
+    info.append(infoH, phone, email);
+    location.append(locationH, locationS);
+    container.append(location, info);
+
+    return container
+})();
+
+const contact = (() => {
+    const container = document.createElement('div');
+    
+    container.setAttribute('id', 'contact');
+    container.classList.add('tab');
+
+    container.append(hero, content);
+
+    return container;
+})();
+
+export {contact}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import {heading} from './components/heading.js';
 import {home} from './components/home.js';
 import {menu} from './components/menu.js';
-
+import {contact} from './components/contact.js';
 
 const content = document.getElementById('content')
 const load = (() => {
     content.appendChild(heading);
     content.appendChild(home);
     content.appendChild(menu);
+    content.appendChild(contact);
 })();
 
 load
